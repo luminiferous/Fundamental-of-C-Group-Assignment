9b71af3c1e10d6ca06d5035f561732cdfa640f6e
pseudoptics <cameronwang2000@gmail.com>
2019-09-19 23:56:00 +1000

    Updated README.md / Updated assignment.c with function prototypes and block comments.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# Fundamental-of-C-Group-Assignment
-Assignment for Fundamental of c. 
-
+# Fundamentals of C Programming - Assessment 3
+## Project Name: Secure Restaurant Database System (Placeholder)
+**Authors** - Cameron Wang (13202320), Brendan Huynh, Peter Phan,
+David Ung, Daming Luo
--- assignment.c
+++ assignment.c
@@ -1,6 +1,30 @@
-include <stdio.h> 
+/***************************************************************
+Fundamentals of C Programming - Assessment 3
 
-int main(void){
+Project Name: Secure Restaurant Database System (Placeholder)
+Authors: Cameron Wang (13202320), Brendan Huynh, Peter Phan,
+          David Ung, Daming Luo
+Date of Submission: -
+***************************************************************/
+#include <stdio.h>
 
-  return 0; 
+/***************************************************************
+Function Prototypes
+**NOTE** - At current stage in development, function prototypes
+            are subject to change.
+***************************************************************/
+void menu(void);
+void customer_detail_input(void);
+void display_all_customers(void);
+int encrypt_customer_password(char[] database_file, char[] customer_password);
+int decrypt_customer_password(char[] database_file, char[] customer);
+int compress_database_file(char[] database_file);
+int decompress_database_file(char[] compressed_database_file);
+
+/***************************************************************
+Main Function
+***************************************************************/
+int main(void)
+{
+  return 0;
 }
